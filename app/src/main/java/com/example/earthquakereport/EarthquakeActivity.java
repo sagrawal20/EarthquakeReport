81d3eb769badb7f207ecae1349434f4ea449c766
saahilagrawal <saahil.agrawal@gmail.com>
2023-02-28 10:13:26 +0530

    Small Changes Updated

--- app/src/main/java/com/example/earthquakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/earthquakereport/EarthquakeActivity.java
@@ -36,8 +36,6 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-//        String earthquakesQuery = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1672944770000,\"url\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&limit=1\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.13.6\",\"limit\":1,\"offset\":1,\"count\":1},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":1.29,\"place\":\"10km SSW of Idyllwild, CA\",\"time\":1388620296020,\"updated\":1457728844428,\"tz\":null,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/ci11408890\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=ci11408890&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":null,\"alert\":null,\"status\":\"reviewed\",\"tsunami\":0,\"sig\":26,\"net\":\"ci\",\"code\":\"11408890\",\"ids\":\",ci11408890,\",\"sources\":\",ci,\",\"types\":\",cap,focal-mechanism,nearby-cities,origin,phase-data,scitech-link,\",\"nst\":39,\"dmin\":0.06729,\"rms\":0.09,\"gap\":51,\"magType\":\"ml\",\"type\":\"earthquake\",\"title\":\"M 1.3 - 10km SSW of Idyllwild, CA\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-116.7776667,33.6633333,11.008]},\"id\":\"ci11408890\"}]}";
-
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
@@ -68,7 +66,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         /**
          * This method runs on a background thread and performs the network request.
          * We should not update the UI from a background thread, so we return a list of
-         * {@link Earthquake}s as the result.
+         * {@link }s as the result.
          */
         @Override
         protected ArrayList<Data> doInBackground(String... urls) {
